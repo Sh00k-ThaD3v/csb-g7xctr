3e06d8f90305d4104461aff3dd92673068d50e60
Code Sandbox <hello@codesandbox.io>
2022-05-07 09:58:04 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b0bd4152de1fc005be623d6a33aaae30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7029798c1f5fe239e33a5b86ae3812ea",
+    "url": "https://sh00k-thad3v.github.io/csb-g7xctr/index.html"
+  },
+  {
+    "revision": "f0cb11fa3da13737444d",
+    "url": "https://sh00k-thad3v.github.io/csb-g7xctr/static/css/main.801207de.chunk.css"
+  },
+  {
+    "revision": "13aefb30dda1ff004f1b",
+    "url": "https://sh00k-thad3v.github.io/csb-g7xctr/static/js/2.c5220e68.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "https://sh00k-thad3v.github.io/csb-g7xctr/static/js/2.c5220e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f536baa33e38ea6463db",
+    "url": "https://sh00k-thad3v.github.io/csb-g7xctr/static/js/3.e9a94fb1.chunk.js"
+  },
+  {
+    "revision": "f0cb11fa3da13737444d",
+    "url": "https://sh00k-thad3v.github.io/csb-g7xctr/static/js/main.39382cf7.chunk.js"
+  },
+  {
+    "revision": "1032188492ff2f3e3cf9",
+    "url": "https://sh00k-thad3v.github.io/csb-g7xctr/static/js/runtime-main.93c3cccf.js"
+  }
+]);
